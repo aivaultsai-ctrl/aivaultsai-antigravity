a69f1eb18beefce5691456ae959b220d9a7bfbd2
Stephan Meester <stephanmeester8@gmail.com>
2025-12-13 02:25:27 +0100

    fix: add zod/v3 alias for webpack and turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "zod/v3": "zod",
+    };
+    return config;
+  },
+  experimental: {
+    // @ts-ignore
+    turbo: {
+      resolveAlias: {
+        "zod/v3": "zod",
+      },
+    },
+  },
 };
 
 export default nextConfig;

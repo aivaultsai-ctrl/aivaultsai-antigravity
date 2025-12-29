11911e8e7961d03a0ef5a1032bb1d43902857ab3
Stephan Meester <stephanmeester8@gmail.com>
2025-12-29 08:31:42 +0100

    chore: remove firebase build artifacts from git

--- .firebase/clipvaultapi/functions/.next/static/chunks/app/(dashboard)/agents/layout-9faaacf37ef411be.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185,308,486,568],{2551:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2551)}),_N_E=n.O()}]);
